e18ffd0fc4dbdca2543f473cad9357f731f5ba93
jessie <jessie@webglsoft.com>
2023-07-06 00:58:25 +0800

    build:  vite-plugin-svg-icons

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import '@/styles/index.scss'
 
 import router from '@/router'
 import pinia from '@/store'
-
+import 'virtual:svg-icons-register'
+import gloablComponent from '@/components'
 const app = createApp(App)
 
+app.use(gloablComponent)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
